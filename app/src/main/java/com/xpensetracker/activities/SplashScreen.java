d371644d0cb9dff2d839575225922d8683d88a02
Prashant <pk021998@gmail.com>
2019-11-28 03:46:23 +0530

    Custom Fonts | Style changed

---
+++ app/src/main/java/com/xpensetracker/activities/SplashScreen.java
@@ -0,0 +1,29 @@
+package com.xpensetracker.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import com.xpensetracker.R;
+
+public class SplashScreen extends AppCompatActivity {
+
+    private static final int SPLASH_TIME = 10000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(SplashScreen.this, MainActivity.class));
+                finish();
+            }
+        }, SPLASH_TIME);
+
+    }
+}
